2b507902e613a8af3c38f6d6103d91867f1c1f8f
KvltKitty <jherzenach@mail.csuchico.edu>
2015-02-22 07:10:15 -0800

    Added some other music
    for the second scene

---
+++ src/Assets/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("BOOM");
+		int i = Application.loadedLevel;
+		Application.LoadLevel (i + 1);
+	}
+}
---
+++ src/Assets/hintText.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hintText : MonoBehaviour {
+
+
+	bool hitBox;
+	Color color;
+	GUIStyle myStyle;
+	
+	void Start(){
+		hitBox = false;
+		guiText.enabled = false;
+		color = transform.guiText.color;
+	}
+	
+	void Update(){
+
+		if(hitBox){
+			Fade();
+		}
+	}
+
+
+	void OnGUI(){
+		if(hitBox){
+			guiText.material.color = color;
+			guiText.enabled = true;
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.tag.Equals("Player")){
+			hitBox = true;
+			Debug.Log ("BOOM");
+		}
+	}
+	void Fade(){
+		
+		if (transform.guiText.material.color.a > 0){
+			color = transform.guiText.material.color;
+			color.a -= 0.1f* Time.deltaTime;
+			transform.guiText.material.color = color;
+
+		}
+	}
+}
