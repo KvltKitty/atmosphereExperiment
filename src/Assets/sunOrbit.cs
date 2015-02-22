b947ec7aeccdad0b76e0bf57bed53e600df5d3d7
KvltKitty <jherzenach@mail.csuchico.edu>
2015-02-22 04:41:57 -0800

    Added some suns to the "space!" room

---
+++ src/Assets/sunOrbit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sunOrbit : MonoBehaviour {
+	public enum direction{
+		right,
+		left,
+		up,
+		down
+	}
+	public direction dir;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(dir == direction.right){
+		transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
+		}
+		if(dir == direction.left){
+			transform.RotateAround(Vector3.zero, Vector3.left, -speed * Time.deltaTime);
+		}
+		if(dir == direction.up){
+			transform.RotateAround(Vector3.zero, new Vector3(1.0f, 0.0f, 1.0f), speed * Time.deltaTime);
+		}
+		if(dir == direction.down){
+			transform.RotateAround(Vector3.zero, new Vector3(1.0f, 0.0f, -1.0f), speed * Time.deltaTime);
+		}
+	}
+}
